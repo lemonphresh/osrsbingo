e662d7a837d6836d0c7bcb1728f2f6b85c057f60
Lemon Garrett <lgarrett@legacy.com>
2024-12-09 17:55:58 -0800

    use DATABASE_URL env var

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,21 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('osrsbingo', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: console.log, // Enable SQL query logging
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: console.log,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize('osrsbingo', 'postgres', 'password', {
+      host: 'localhost',
+      dialect: 'postgres',
+      logging: console.log,
+    });
 
 module.exports = sequelize;
