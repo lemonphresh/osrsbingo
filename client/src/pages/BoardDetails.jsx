6031ac279401f6e3b1717cfb7112ab9311d21a1b
Lemon Garrett <lgarrett@legacy.com>
2025-01-10 08:20:25 -0800

    deps

--- client/src/pages/BoardDetails.jsx
+++ client/src/pages/BoardDetails.jsx
@@ -228,7 +228,7 @@ const BoardDetails = () => {
         }
       }
     }
-  }, [data, loading, navigate, user?.admin]);
+  }, [data, isEditor, loading, navigate, user?.admin]);
 
   return (
     <Flex
