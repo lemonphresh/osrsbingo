fc3f84f8d1bcbc0b0b8dd3ee12eac071c542bf0e
lemon <lemon.tsx@gmail.com>
2026-02-28 17:34:09 -0800

    suppress warnings on tests that dont matter

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -9,6 +9,7 @@ import '@testing-library/jest-dom';
 const originalConsoleError = console.error;
 console.error = (...args) => {
   if (typeof args[0] === 'string' && args[0].includes('React does not recognize the')) return;
+  if (typeof args[0] === 'string' && args[0].includes('ReactDOMTestUtils.act')) return;
   originalConsoleError(...args);
 };
 
